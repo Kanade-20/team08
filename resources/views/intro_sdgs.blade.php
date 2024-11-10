8d54464c011254cbda31028b81484213ccae5f0b
Wan Jun <D1124182712@gm.lhu.edu.tw>
2024-11-10 23:52:29 +0800

    SDG小組作業1最終版

--- resources/views/intro_sdgs.blade.php
+++ resources/views/intro_sdgs.blade.php
@@ -18,6 +18,30 @@
             padding: 20px;
             text-align: center;
         }
+        ul.issue {
+            list-style-type: none;
+            padding: 0;
+            border-radius: 10px; 
+            padding: 15px; 
+            background-color: rgb(241, 241, 241);  
+        }
+        
+        li.issue_title{
+            border-radius: 10px;
+            font-size: 150%;
+            font-weight: bold;
+        }
+        
+        img{
+            height:268px;
+            width:400px;
+        }
+        
+        p.issue_content::before {
+            content: '• '; 
+            color: black; 
+            font-size: 1.2em;
+        }
 
         main {
             padding: 20px;
@@ -25,18 +49,6 @@
 
         h1, h2 {
             color: #333;
-        }
-
-        ul {
-            list-style-type: none;
-            padding: 0;
-        }
-
-        ul li {
-            background: #f4f4f4;
-            margin: 5px 0;
-            padding: 10px;
-            border-radius: 5px;
         }
 
         footer {
@@ -62,23 +74,93 @@
         </section>
         <section>
             <h2>17個可持續發展目標</h2>
-            <ul>
-                <li>無貧窮</li>
-                <li>零饑餓</li>
-                <li>健康與福祉</li>
-                <li>優質教育</li>
-                <li>性別平等</li>
-                <li>清潔水與衛生</li>
-                <li>經濟增長與體面工作</li>
-                <li>產業、創新與基礎設施</li>
-                <li>減少不平等</li>
-                <li>可持續城市與社區</li>
-                <li>負責任的消費與生產</li>
-                <li>氣候行動</li>
-                <li>海洋生態</li>
-                <li>陸地生態</li>
-                <li>和平與正義</li>
-                <li>夥伴關係</li>
+            <h2>(點選圖片可以了解更多)</h2>
+            <ul class="issue" >    
+                <li class="issue_title">1無貧窮</li>
+                <p class="issue_content">消除一切形式的貧困，特別是極端貧困。</p>
+                <p class="issue_content">提高社會保障的普及性，保障所有人群的基本需求。</p>
+                <p class="issue_content">促進公平、可持續的經濟增長。</p>
+                <a href="https://globalgoals.tw/1-no-poverty"><img src="https://storage.googleapis.com/futurecity-cms-cwg-tw/ckeditor/202101/ckeditor-6010d440e1ecf.jpg" /></a>   
+                <li class="issue_title">2零饑餓</li>
+                <p class="issue_content">消除全球飢餓，確保所有人享有足夠營養。</p>
+                <p class="issue_content">提高農業生產力，促進可持續農業。</p>
+                <p class="issue_content">改善弱勢群體的食品安全與營養。</p>
+                <a href="https://globalgoals.tw/2-zero-hunger"><img src="https://storage.googleapis.com/futurecity-cms-cwg-tw/ckeditor/202101/ckeditor-6010d482a7718.jpg" /></a>
+                <li class="issue_title">3健康與福祉Good Health and Well-being</li>
+                <p class="issue_content">確保人人享有健康的生活並促進福祉。</p>
+                <p class="issue_content">降低全球死亡率，尤其是嬰兒和母親死亡率。</p>
+                <p class="issue_content">提高精神健康服務的可得性與質量。</p>   
+                <a href="https://globalgoals.tw/3-good-health-and-well-being"><img src="https://storage.googleapis.com/futurecity-cms-cwg-tw/ckeditor/202101/ckeditor-6010d4bfbd6f6.jpg" /></a>
+                <li class="issue_title">4優質教育Quality Education</li>
+                <p class="issue_content">提供免費、公平且具有包容性的優質教育。</p>
+                <p class="issue_content">增加青年與成人的技能培訓機會。</p>
+                <p class="issue_content">促進終身學習的機會，確保教育公平。</p>
+                <a href="https://globalgoals.tw/4-quality-education"><img src="https://storage.googleapis.com/futurecity-cms-cwg-tw/ckeditor/202101/ckeditor-6010d50654af8.jpg "/></a>
+                <li class="issue_title">5性別平等Gender Equality</li>                
+                <p class="issue_content">消除所有形式的性別歧視與暴力。</p>
+                <p class="issue_content">確保女性在領導、決策及政治參與中的平等權利。</p>
+                <p class="issue_content">促進女性和女孩的教育與經濟機會。</p> 
+                <a href="https://globalgoals.tw/5-gender-equality"><img src="https://storage.googleapis.com/futurecity-cms-cwg-tw/ckeditor/202101/ckeditor-6010d550ad341.jpg" /></a>
+                <li class="issue_title">6清潔水與衛生Clean Water and Sanitation</li>        
+                <p class="issue_content">確保所有人都能享有清潔飲用水與衛生設施。</p>
+                <p class="issue_content">減少水資源的污染和過度消耗。</p>
+                <p class="issue_content">促進水資源管理的可持續性。</p> 
+                <a href="https://globalgoals.tw/6-clean-water-and-sanitation"><img src="https://storage.googleapis.com/futurecity-cms-cwg-tw/ckeditor/202101/ckeditor-6010d5830066a.jpg" /></a>
+                <li class="issue_title">7可負擔的永續能源 (Affordable and Clean Energy)</li>
+                <p class="issue_content">確保每個人都能夠獲得可負擔且可靠的現代能源。</p>
+                <p class="issue_content">促進可再生能源的使用，減少對化石燃料的依賴。</p>
+                <p class="issue_content">改善能源效率，推動低碳技術的發展。</p> 
+                <a href="https://globalgoals.tw/7-affordable-and-clean-energy"><img src="https://storage.googleapis.com/futurecity-cms-cwg-tw/ckeditor/202101/ckeditor-6010d751c176f.jpg" /></a>
+                <li class="issue_title">8經濟增長與體面工作Decent Work and Economic Growth</li>                
+                <p class="issue_content">促進持續、包容和可持續的經濟增長。</p>
+                <p class="issue_content">提高生產力，促進體面工作機會。</p>
+                <p class="issue_content">加強創新、技術和產業發展。</p> 
+                <a href="https://globalgoals.tw/8-decent-work-and-economic-growth"><img src="https://storage.googleapis.com/futurecity-cms-cwg-tw/ckeditor/202101/ckeditor-6010d78c8dd83.jpg" /></a>
+                <li class="issue_title">9產業、創新與基礎設施Industry, Innovation and Infrastructure</li>                
+                <p class="issue_content">建設有韌性和可持續的基礎設施。</p>
+                <p class="issue_content">促進創新與技術發展。</p>
+                <p class="issue_content">提高工業化進程，尤其是在發展中國家。</p> 
+                <a href="https://globalgoals.tw/9-industry-innovation-and-infrastructure"><img src="https://storage.googleapis.com/futurecity-cms-cwg-tw/ckeditor/202101/ckeditor-6010d7ba19abe.jpg" /></a>
+                <li class="issue_title">10減少不平等Reduced Inequality</li>                
+                <p class="issue_content">促進社會、經濟和政治的包容性。</p>
+                <p class="issue_content">減少國內和國際間的不平等。</p>
+                <p class="issue_content">確保全體人民擁有平等的機會。</p> 
+                <a href="https://globalgoals.tw/10-reduced-inequalities"><img src="https://storage.googleapis.com/futurecity-cms-cwg-tw/ckeditor/202101/ckeditor-6010d7ee9825f.jpg" /></a>
+                <li class="issue_title">11可持續城市與社區Sustainable Cities and Communities</li>                
+                <p class="issue_content">促進城市的可持續發展，改善城市生活質量。</p>
+                <p class="issue_content">提高基礎設施的可持續性和韌性。</p>
+                <p class="issue_content">促進社區參與與公平的城市發展。</p> 
+                <a href="https://globalgoals.tw/11-sustainable-cities-and-communities"><img src="https://storage.googleapis.com/futurecity-cms-cwg-tw/ckeditor/202101/ckeditor-6010d82775064.jpg" /></a>
+                <li class="issue_title">12負責任的消費與生產Responsible Consumption and Production</li>
+                <p class="issue_content">促進資源的可持續管理與減少浪費。</p>
+                <p class="issue_content">鼓勵企業和消費者改變消費模式，降低環境影響。</p>
+                <p class="issue_content">強化可持續的生產與供應鏈管理。</p> 
+                <a href="https://globalgoals.tw/12-responsible-consumption-and-production"><img src="https://storage.googleapis.com/futurecity-cms-cwg-tw/ckeditor/202101/ckeditor-6010d853f2f50.jpg" /></a>
+                <li class="issue_title">13氣候行動Climate Action</li>
+                <p class="issue_content">減少溫室氣體排放，應對氣候變遷的挑戰。</p>
+                <p class="issue_content">促進低碳發展和綠色能源的使用。</p>
+                <p class="issue_content">提高社會和生態系統的抗災能力。</p> 
+                <a href="https://globalgoals.tw/13-climate-action"><img src="https://storage.googleapis.com/futurecity-cms-cwg-tw/ckeditor/202101/ckeditor-6010d89ee3c07.jpg" /></a>    
+                <li class="issue_title">14海洋生態(Life Below Water)</li>                
+                <p class="issue_content">保護海洋生態系統和生物多樣性。</p>
+                <p class="issue_content">減少海洋污染，特別是塑料垃圾。</p>
+                <p class="issue_content">加強漁業管理與可持續捕撈。</p> 
+                <a href="https://globalgoals.tw/14-life-below-water"><img src="https://storage.googleapis.com/futurecity-cms-cwg-tw/ckeditor/202101/ckeditor-6010d8c71bf04.jpg" /></a>
+                <li class="issue_title">15陸地生態(Life on Land)</li>
+                <p class="issue_content">保護陸地生態系統和森林。</p>
+                <p class="issue_content">促進生物多樣性和自然資源的可持續利用。</p>
+                <p class="issue_content">防止土地退化和沙漠化。</p> 
+                <a href="https://globalgoals.tw/15-life-on-land"><img src="https://storage.googleapis.com/futurecity-cms-cwg-tw/ckeditor/202101/ckeditor-6010d8fd7986e.jpg" /></a>
+                <li class="issue_title">16和平與正義(Peace, Justice and Strong Institutions)</li>
+                <p class="issue_content">促進公正、和平的社會，減少暴力與衝突。</p>
+                <p class="issue_content">提高司法體系的可得性與公正性。</p>
+                <p class="issue_content">強化國家與國際機構的透明度和責任感。</p>
+                <a href="https://globalgoals.tw/16-peace-justice-and-strong-institutions"><img src="https://storage.googleapis.com/futurecity-cms-cwg-tw/ckeditor/202101/ckeditor-6010d93643593.jpg" /></a> 
+                <li class="issue_title">17夥伴關係(Partnerships for the Goals)</li>                
+                <p class="issue_content">建立全球合作夥伴關係來促進可持續發展。</p>
+                <p class="issue_content">強化發展資源的分配與分享。</p>
+                <p class="issue_content">促進多方合作以解決全球問題。</p> 
+                <a href="https://globalgoals.tw/17-partnerships-for-the-goals"><img src="https://storage.googleapis.com/futurecity-cms-cwg-tw/ckeditor/202101/ckeditor-6010d96614ddb.jpg" /></a>
             </ul>
         </section>
         <section>
